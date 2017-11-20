b493dbfe0457497f161380e8e0459d123d05970a
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2017-11-20 13:14:19 -0500

    pushy

--- Lab3.2/src/ArrayMethods2.java
+++ Lab3.2/src/ArrayMethods2.java
@@ -2,27 +2,47 @@
 public class ArrayMethods2 {
 	public static void main (String args[]) 
 	{
-		String[] temp1 = {"apple","banana","cat","dog","elephant"};
-		String[] temp2 = {"cat","corgi","for","my","young","zebra"};
+		String[] temp1 = {"zebra","banana","cat","dog","elephant"};
+		String[] temp2 = {"zebra","bong","my","for","young","zebra"};
 		printArray(merge(temp1, temp2));
 	}
 	public static String[] merge(String[] list1, String[]list2) 
 	{
 		String[] merged = new String[list1.length + list2.length];
-		for(int j = 0; j < list1.length; j++){
-			for(int k = j; k < list2.length; k++){
-				for(int i = 0; i < (list1.length + list2.length); i++){
-					if(list1[j].substring(0, 1).compareTo(list2[k].substring(0, 1)) > 0) {
-						merged[i] = list1[j];
+		String[] t1 = copyArray(list1);
+		String[] t2 = copyArray(list2);
+		for(int i = 0; i < list1.length; i++)
+		{
+			merged[i] = t1[i];
+		}
+		for(int n = list1.length; n < merged.length; n++)
+		{
+			merged[n] = t2[n - list1.length];
+		}
+		String[] m1 = copyArray(merged);
+		boolean condition = false;
+		while(!condition)
+		{
+			for(int l = 0; l < m1.length-1; l++)
+			{
+				condition = true;
+				if(m1[l].compareTo(m1[l+1]) > 0)
+				{
+					System.out.println("Switching:" +m1[l]+" and "+m1[l+1]);
+					String temp = m1[l];
+					m1[l] = m1[l + 1];
+					m1[l + 1] = temp;
+					condition = false;
 				}
-					if(list1[j].substring(0, 1).compareTo(list2[k].substring(0, 1)) < 0) {
-						merged[i] = list2[k];
-				}
-				merged[i]= list1[j];
 			}
 		}
+		return m1;
 	}
-		return merged;
+	public static void swap(String[] arr, int i, int j)
+	{
+		String temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 	public static String[] mergeSort(String[] list)
 	{
@@ -32,9 +52,9 @@ public class ArrayMethods2 {
 	{
 		return 0;
 	}
-	public 	static int[] copyArray(int[] intArray)
+	public 	static String[] copyArray(String[] intArray)
 	{
-		int[] copy = new int[intArray.length];
+		String[] copy = new String[intArray.length];
 		for(int i = 0; i < intArray.length; i++)
 		{
 			copy[i] = intArray[i];
